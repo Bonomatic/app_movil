12769966b64dfd8fda7430b18d09189b14082274
Julian Ceballos Henao <julianc.ceballos@hotmail.com>
2016-07-31 10:17:36 -0500

    Pagina Registro

---
+++ Bonomatic/app/src/main/java/com/bonomatic/bonomatic/MainActivity.java
@@ -0,0 +1,35 @@
+package com.bonomatic.bonomatic;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
+
+
+    }
+
+    @OnClick(R.id.btnRegEnviar)
+    public void enviar(View view){
+        Snackbar.make(view, "Prueba Snack", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+    }
+
+
+}
